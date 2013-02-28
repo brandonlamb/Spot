2673f5d5a41ff07d882e02bb1ee0c43c3828c76d
Brandon Lamb <brandon@brandonlamb.com>
2013-02-27 23:18:00 -0800

    blah

--- lib/Spot/Type/String.php
+++ lib/Spot/Type/String.php
@@ -4,30 +4,27 @@ use Spot\Entity;
 
 class String implements TypeInterface
 {
-    /**
-     * Cast given value to type required
-     */
-    public static function cast($value)
-    {
-        if(null !== $value) {
-            return (string) $value;
-        }
-        return $value;
-    }
+	/**
+	 * Cast given value to type required
+	 */
+	public static function cast($value)
+	{
+		return (null !== $value) ? (string) $value : $value;
+	}
 
-    /**
-     * Geting value off Entity object
-     */
-    public static function get(Entity $entity, $value)
-    {
-        return self::cast($value);
-    }
+	/**
+	 * Geting value off Entity object
+	 */
+	public static function get(Entity $entity, $value)
+	{
+		return self::cast($value);
+	}
 
-    /**
-     * Setting value on Entity object
-     */
-    public static function set(Entity $entity, $value)
-    {
-        return self::cast($value);
-    }
+	/**
+	 * Setting value on Entity object
+	 */
+	public static function set(Entity $entity, $value)
+	{
+		return self::cast($value);
+	}
 }
